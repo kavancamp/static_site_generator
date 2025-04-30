aae7784f289b2379bde201d0315bef8333cbaee0
kavancamp <keenahvancampenhout@gmail.com>
2025-04-30 18:04:34 -0500

    added main and call to textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(node)
+
+
+main()
