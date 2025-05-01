168a39587a819b228ed696dc55c74b439b8dfa07
kavancamp <keenahvancampenhout@gmail.com>
2025-05-01 18:03:40 -0500

    fixed props

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,19 +3,22 @@ class HTMLNode:
         self.tag = tag
         self.value = value
         self.children = children
-        self.props = props if props is not None else {}
+        self.props = props
 
 
     def to_html(self):
         raise NotImplementedError("This function is not yet implemented.")
     
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
-        return " ".join(f'{key}="{value}"' for key, value in sorted(self.props.items()))
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
     
     def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
     
 
     #node = HTMLNode(
@@ -28,41 +31,7 @@ class HTMLNode:
 
 #print(node.props_to_html())
 #print(node.__repr__())
-class LeafNode(HTMLNode):
-    def __init__(self, tag, value,  props=None):
-        if value is None:
-            raise ValueError("LeafNode must have a value.")
-        super().__init__(tag=tag, value=value, children=None, props=props)
-        
-    def to_html(self):
-        if self.value is None:
-            raise ValueError("LeafNode must have a value.")
-        if self.tag is None:
-            return self.value
-        else:
-            props = self.props_to_html()
-            space = " " if props else ""
-            return f"<{self.tag}{space}{props}>{self.value}</{self.tag}>"
-    
-#node1 = LeafNode("p", "This is a paragraph.")
-#print(node1.to_html())
-#node2 = LeafNode("a", "Click here", {"href": "https://www.example.com", "target": "_blank"})
-#print(node2.to_html())
-#node3 = LeafNode(None, "Just some raw text")
-#print(node3.to_html())
 
-class ParentNode(HTMLNode):
-    
-    def __init__(self, tag, children, props=None):
-        if tag is None or children is None:
-            raise ValueError("Parent Node must have a tag, and children.")
-        super().__init__(tag=tag, children=children, props=props)
-    def to_html(self):
-            if self.value is None:
-                raise ValueError("LeafNode must have a value.")
-            if self.tag is None:
-                return self.value
-            else:
-                props = self.props_to_html()
-                space = " " if props else ""
-                return f"<{self.tag}{space}{props}>{self.value}</{self.tag}>"
+
+
+
