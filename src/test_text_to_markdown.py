d1ee95729a09672687d609aa03b9e0f3d6497115
kavancamp <keenahvancampenhout@gmail.com>
2025-05-02 16:43:25 -0500

    added test textnodes

---
+++ src/test_text_to_markdown.py
@@ -0,0 +1,41 @@
+import unittest
+from texttotextnode import text_to_textnodes
+from textnode import TextNode, TextType
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_mixed_formatting(self):
+        input_text = (
+            "This is **text** with an _italic_ word and a `code block` "
+            "and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        actual = text_to_textnodes(input_text)
+        self.assertEqual(expected, actual)
+
+    def test_no_formatting(self):
+        input_text = "Just a simple line of text."
+        expected = [TextNode("Just a simple line of text.", TextType.TEXT)]
+        self.assertEqual(text_to_textnodes(input_text), expected)
+
+    def test_only_bold(self):
+        input_text = "This is **bold** text."
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text.", TextType.TEXT),
+        ]
+        self.assertEqual(text_to_textnodes(input_text), expected)
+
+if __name__ == '__main__':
+    unittest.main()
