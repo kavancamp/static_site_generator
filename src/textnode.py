00177d2671d6b70c41a3c503d4a23d76e26dbc8e
kavancamp <keenahvancampenhout@gmail.com>
2025-05-02 16:44:18 -0500

    added text node to htm method

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -23,3 +24,29 @@ class TextNode:
         )
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node.text_type, TextType):
+        raise Exception(f"Invalid text_type: {text_node.text_type}")
+    
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        if not text_node.url:
+            raise ValueError("Link text nodes must have a URL")
+        return LeafNode("a", text_node.text, props={"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        if not text_node.url:
+            raise ValueError("Image text nodes must have a url")
+        return LeafNode("img", "", props={"src": text_node.url, "alt": text_node.text})
+    else:
+        raise ValueError(f"Unsupported text type: {text_node.text_type}")
+
+
